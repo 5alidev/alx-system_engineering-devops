fca01d4628cfb3cda3d3dd949c889910bf7d56d8
5alidev <khalidbenhamou.dev@outlook.com>
2024-04-19 11:08:36 -0700

    istall packages: python, flask, Werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,14 @@
 #  install flask from pip3.
 
+package { 'python3':
+  ensure => '3.8.10',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
