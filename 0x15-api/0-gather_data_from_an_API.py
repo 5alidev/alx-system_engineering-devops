5b071a66c814537e39a45b61a7a4662401b7f165
5alidev <khalidbenhamou.dev@outlook.com>
2024-05-20 16:35:36 -0700

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Gather data from an API
+"""
+import requests
+import sys
+
+url = 'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1])
+usrs = requests.get(url)
+usrsJson = usrs.json()
+name = usrsJson["name"]
+nbr_t_done = 0
+total_t = 0
+
+todosURL = 'https://jsonplaceholder.typicode.com/todos'
+todos = requests.get(todosURL)
+todosJson = todos.json()
+
+for t in todosJson:
+    if t["userId"] == int(sys.argv[1]):
+        total_t += 1
+        if t["completed"]:
+            nbr_t_done += 1
+
+print('Employee {} is done with tasks({}/{}):'
+      .format(name, nbr_t_done, total_t))
+
+print('\n'.join("\t " + t["title"] for t in todosJson
+      if t['userId'] == int(sys.argv[1]) and t['completed']))
