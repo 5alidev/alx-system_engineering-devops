b9ddd0ce2f6a3c6091741d3d1c150cf36b9839a5
5alidev <khalidbenhamou.dev@outlook.com>
2024-05-20 16:41:00 -0700

    Module is documented

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
 """
-Gather data from an API
+for a given employee ID, returns information about
+his/her TODO list progress
 """
 import requests
 import sys
 
-url = 'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1])
-usrs = requests.get(url)
-usrsJson = usrs.json()
-name = usrsJson["name"]
-nbr_t_done = 0
-total_t = 0
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1])
+    usrs = requests.get(url)
+    usrsJson = usrs.json()
+    name = usrsJson["name"]
+    nbr_t_done = 0
+    total_t = 0
 
-todosURL = 'https://jsonplaceholder.typicode.com/todos'
-todos = requests.get(todosURL)
-todosJson = todos.json()
+    todosURL = 'https://jsonplaceholder.typicode.com/todos'
+    todos = requests.get(todosURL)
+    todosJson = todos.json()
 
-for t in todosJson:
-    if t["userId"] == int(sys.argv[1]):
-        total_t += 1
-        if t["completed"]:
-            nbr_t_done += 1
+    for t in todosJson:
+        if t["userId"] == int(sys.argv[1]):
+            total_t += 1
+            if t["completed"]:
+                nbr_t_done += 1
 
-print('Employee {} is done with tasks({}/{}):'
-      .format(name, nbr_t_done, total_t))
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, nbr_t_done, total_t))
 
-print('\n'.join("\t " + t["title"] for t in todosJson
-      if t['userId'] == int(sys.argv[1]) and t['completed']))
+    print('\n'.join("\t " + t["title"] for t in todosJson
+          if t['userId'] == int(sys.argv[1]) and t['completed']))
