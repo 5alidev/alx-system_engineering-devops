e559797ac6a66f8f1638e55eb384b4657d558eae
5alidev <khalidbenhamou.dev@outlook.com>
2024-05-20 16:54:27 -0700

    change code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,25 +7,30 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1])
+    url = 'https://jsonplaceholder.typicode.com/users/'
     usrs = requests.get(url)
     usrsJson = usrs.json()
-    name = usrsJson["name"]
+
+    for u in usrsJson:
+        if u.get("id") == int(sys.argv[1]):
+            name = u.get('name')
     nbr_t_done = 0
     total_t = 0
 
     todosURL = 'https://jsonplaceholder.typicode.com/todos'
     todos = requests.get(todosURL)
     todosJson = todos.json()
+    tasks = []
 
     for t in todosJson:
         if t["userId"] == int(sys.argv[1]):
             total_t += 1
-            if t["completed"]:
+            if t.get("completed"):
                 nbr_t_done += 1
+                tasks.append(t.get('title'))
 
     print('Employee {} is done with tasks({}/{}):'
           .format(name, nbr_t_done, total_t))
 
-    print('\n'.join("\t " + t["title"] for t in todosJson
-          if t['userId'] == int(sys.argv[1]) and t['completed']))
+    for task in tasks:
+        print("\t {}".format(task))
