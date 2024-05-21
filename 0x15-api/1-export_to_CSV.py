9bede34cd739c2de0f01bc68da47530472ba67cb
5alidev <khalidbenhamou.dev@outlook.com>
2024-05-21 04:41:06 -0700

    csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,21 +3,25 @@
 for a given employee ID, returns information about
 his/her TODO list progress
 """
+
 import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-    USER_ID = sys.argv[1]
-    user = requests.get(url + "users/{}".format(USER_ID)).json()
-    USERNAME = user.get("username")
 
-    params = {"userId": USER_ID}
-    todos = requests.get(url + "todos", params).json()
+if __name__ == "__main__":
+    USER_ID = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/' + USER_ID
+    userRes = requests.get(url)
+
+    USERNAME = userRes.json().get("username")
+    todosUrl = url + '/todos'
+    todoRes = requests.get(todosUrl)
+    tasks = todoRes.json()
 
     with open("{}.csv".format(USER_ID), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [USER_ID, USERNAME, t.get("completed"), t.get("title")]
-        ) for t in todos]
+        for task in tasks:
+            completed = task.get('completed')
+            title = task.get('title')
+            csvfile.write('"{}","{}","{}","{}"\n'
+                          .format(USER_ID, USERNAME, completed, title))
